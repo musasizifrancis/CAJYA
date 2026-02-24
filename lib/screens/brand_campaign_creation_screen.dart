b9ec9caf3761ca210be1beb5237a19ad663ee3cd
MUSASIZI FRANCIS <musasizifrancis@gmail.com>
2026-02-24 15:30:06 +0300

    Fix: Correct import path for ApiService in brand_campaign_creation_screen.dart

--- lib/screens/brand_campaign_creation_screen.dart
+++ lib/screens/brand_campaign_creation_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'api_service.dart';
+import '../services/api_service.dart';
 
 class BrandCampaignCreationScreen extends StatefulWidget {
   final String? email;
